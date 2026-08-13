afc0b1d16de35af6884a5a1bf7c58176af6740bc
Liu Zheng <philiptrip1975@gmail.com>
2026-08-13 08:22:52 +0800

    Initial Commit

---
+++ SubscriptionTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SubscriptionTracker
+//
+//  Created by 1 on 2026/8/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SubscriptionTracker/SubscriptionTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionTrackerApp.swift
+//  SubscriptionTracker
+//
+//  Created by 1 on 2026/8/13.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
